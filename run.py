7764549e51a8e54b9df75cf35ca6c48d274a247e
Aaron O'Donoghue <aaronodonoghue2k11@gmail.com>
2023-12-05 12:35:46 +0000

    Add word list and choose word
    Add list of words and choose one at random

--- run.py
+++ run.py
@@ -1,10 +1,25 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import choice
 
 class Hangman():
 
-    pass
+    #List of words to choose from for the game
+    words = ["absurd", "avenue", "bagpipes", "blizzard", "buffalo",
+             "cryptic", "dizzy", "duplex", "embezzle", "equip",
+             "faking", "fixable", "fjord", "flapjack", "galaxy",
+             "galvanize", "gizmo", "hazard", "hyphen", "icebox",
+             "injury", "jackpot", "jaywalking", "jigsaw", "joking",
+             "jukebox", "keyhole", "kiosk", "lengths", "luxury",
+             "matrix", "megahertz", "microwave", "mystery", "nightclub",
+             "onyx", "oxidize", "oxygen", "polka", "psychic",
+             "puzzle", "quartz", "quizzical", "rhubarb", "rickshaw",
+             "scratch", "skiving", "snazzy", "sphinx", "stretch",
+             "subway", "swivel", "syndrome", "topaz", "transgress",
+             "transplant", "twelfth", "unknown", "uptown", "vixen",
+             "vortex", "waltz", "wizard", "zephyr", "zigzag",
+             "zodiac", "zombie"]
+
+    #Pick a word at random from the list
+    chosen_word = random.choice(words);
 
 
 class GameData():
